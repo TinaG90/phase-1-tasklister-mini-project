a2ab1b917c1274d6b5ac78613692be3b2f4d957e
Altenia Gaines <gaines.tina90@gmail.com>
2023-04-23 15:24:04 -0700

    creates ToDO

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault()
+    buildToDo(e.target["new-task-description"].value)
+  })
 });
+
+function buildToDo (todo){
+let p = document.createElement("p")
+let button = document.createElement("button")
+  button.addEventListener("click", handleDelete)
+  button.textContent = "Done"
+  p.textContent = `${todo} `
+  p.appendChild(button)
+  console.log(p)
+  document.querySelector("#main-content").appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
